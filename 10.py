c8d31d793c379143908016db1c1c38cfab3e8e64
Ana Karina <anakarina.soueu@gmail.com>
2024-08-30 21:15:32 -0300

    10.py

---
+++ 10.py
@@ -0,0 +1,7 @@
+#soma para carro
+porcentagem = 1.28
+imposto = 1.45  
+
+custo_fabrica = float( input("Digite o preço de fabrica deste carro na fabrica:")  )
+resultado = custo_fabrica*porcentagem
+print("o preço de venda do carro novo, sera:" , resultado )
