f8c47d552a92111b05a77c9bd996dda080a0e969
Ana Karina <anakarina.soueu@gmail.com>
2024-10-11 21:26:41 -0300

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,10 @@
+#18) Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles.
+
+
+valor1 = int(input("Digite algum valor:"))
+valor2 = int(input("Digite outro valor:"))
+
+if valor1 > valor2:
+    print("O primeiro valor é maior.")
+else:
+    print("O segundo valor é maior.")
